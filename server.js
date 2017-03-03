1e2c46a994d1005c314858540600b0bd951e874e
Jennifer Dooley <jennifer.dooley@colorado.edu>
2017-03-02 20:50:19 -0700

    updated cors settings
    added x-access-token

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ server.use(bodyParser.urlencoded({ extended: true }));
 server.use(expressValidator());
 server.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
   next();
 });
 
