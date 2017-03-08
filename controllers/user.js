1e3d55a3e8b06a0e2bdd742997a5ee320f4331cb
Jen Dooley <Jennifer.Dooley@colorado.edu>
2017-03-08 16:28:26 -0700

    Stupid mistake in users/account/password

--- controllers/user.js
+++ controllers/user.js
@@ -21,7 +21,7 @@ exports.postSignup = (req, res) => {
   const errors = req.validationErrors()
 
   if (errors) {
-    res.json({result:1, error:errors})
+    res.json({result:1, error:errors[0].msg})
     return
   }
 
@@ -323,8 +323,8 @@ exports.postUpdateProfile = (req, res, next) => {
  * JSON res: {result: 0/1, error: "xxx", token:"xxx"}
  */
 exports.postUpdatePassword = (req, res, next) => {
-  req.assert('password', 'Password must be at least 4 characters long').len(4)
-  
+  req.assert('newPassword', 'Password must be at least 4 characters long').len(4)
+
   const errors = req.validationErrors()
 
   if (errors) {
